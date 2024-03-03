89554b84f3b73885607b690294bf6bd2a984f69b
unknown <??>
2024-03-03 06:24:56 -0500

    remote db modification

--- go.mod
+++ go.mod
@@ -6,15 +6,15 @@ require (
 	github.com/bwmarrin/discordgo v0.27.1
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
-	gorm.io/driver/sqlite v1.5.5
+	gorm.io/driver/mysql v1.5.4
 	gorm.io/gorm v1.25.7
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
--- internal/database/db.go
+++ internal/database/db.go
@@ -1,7 +1,7 @@
 package database
 
 import (
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"sbchecker/internal/logger"
 	"sbchecker/models"
@@ -10,19 +10,17 @@ import (
 var DB *gorm.DB
 
 func Initialize() error {
-	db, err := gorm.Open(sqlite.Open("internal/database/sbchecker.db"), &gorm.Config{})
+	dsn := "u49059_BZgf1wENL1:IITg+w=Ark3+TBpNrRLiz5dN@tcp(localhost:3306)/s49059_sbchecker?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		logger.Log.WithError(err).Error("Error opening database")
+		logger.Log.WithError(err).Error("Failed to connect to database")
 		return err
 	}
-
 	DB = db
-
 	err = DB.AutoMigrate(&models.Account{}, &models.Ban{})
 	if err != nil {
 		logger.Log.WithError(err).Error("Error migrating database")
 		return err
 	}
-
 	return nil
 }
