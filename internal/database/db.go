819a1fdf31dec51ebc8331f1b32b5261865b3c2f
bradselph <Bselph512@gmail.com>
2024-03-30 08:23:28 -0400

    update
    Updated database connection string (`dsn`) in `db.go` to include `?parseTime=true` for automatic conversion of MySQL date/time values to `time.Time` in Go. Added `DB_VAR` environment variable in `.env` file to store additional DSN parameters.

--- internal/database/db.go
+++ internal/database/db.go
@@ -18,23 +18,21 @@ func Initialize() error {
 		logger.Log.WithError(err).Error("Error loading .env file")
 		return err
 	}
-
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
+	dbVar := os.Getenv("DB_VAR")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s%s", dbUser, dbPassword, dbHost, dbPort, dbName, dbVar)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		logger.Log.WithError(err).Error("Error opening database")
 		return err
 	}
-
 	DB = db
-
 	err = DB.AutoMigrate(&models.Account{}, &models.Ban{})
 	if err != nil {
 		logger.Log.WithError(err).Error("Error migrating database")
