272630b934f9f64c6e9735c30676f3d10ad28cca
bradselph <Bselph512@gmail.com>
2024-05-17 19:38:24 -0400

    Refactor command registration and deletion***
    ***Add removeaccount package import***
    
    ***Update account choices for removeaccount command***
    
    ***Check account for shadowbans in a goroutine

--- command/accountage/accountage.go
+++ command/accountage/accountage.go
@@ -11,15 +11,11 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-// choices holds the list of account choices.
 var choices []*discordgo.ApplicationCommandOptionChoice
 
 // RegisterCommand registers the "accountage" command for a given guild.
 func RegisterCommand(s *discordgo.Session, guildID string) {
-	// Get all account choices for the guild.
 	choices = services.GetAllChoices(guildID)
-
-	// Define the "accountage" command.
 	commands := []*discordgo.ApplicationCommand{
 		{
 			Name:        "accountage",
@@ -36,14 +32,12 @@ func RegisterCommand(s *discordgo.Session, guildID string) {
 		},
 	}
 
-	// Get existing application commands for the guild.
 	existingCommands, err := s.ApplicationCommands(s.State.User.ID, guildID)
 	if err != nil {
 		logger.Log.WithError(err).Error("Error getting application commands")
 		return
 	}
 
-	// Check if the "accountage" command already exists.
 	var existingCommand *discordgo.ApplicationCommand
 	for _, command := range existingCommands {
 		if command.Name == "accountage" {
@@ -54,7 +48,6 @@ func RegisterCommand(s *discordgo.Session, guildID string) {
 
 	newCommand := commands[0]
 
-	// If the command exists, update it. Otherwise, create a new one.
 	if existingCommand != nil {
 		logger.Log.Info("Updating accountage command")
 		_, err = s.ApplicationCommandEdit(s.State.User.ID, guildID, existingCommand.ID, newCommand)
@@ -74,14 +67,12 @@ func RegisterCommand(s *discordgo.Session, guildID string) {
 
 // UnregisterCommand deletes all application commands for a given guild.
 func UnregisterCommand(s *discordgo.Session, guildID string) {
-	// Get existing application commands for the guild.
 	commands, err := s.ApplicationCommands(s.State.User.ID, guildID)
 	if err != nil {
 		logger.Log.WithError(err).Error("Error getting application commands")
 		return
 	}
 
-	// Delete each command.
 	for _, command := range commands {
 		logger.Log.Infof("Deleting command %s", command.Name)
 		err := s.ApplicationCommandDelete(s.State.User.ID, guildID, command.ID)
@@ -95,7 +86,6 @@ func UnregisterCommand(s *discordgo.Session, guildID string) {
 // CommandAccountAge handles the "accountage" command.
 func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	logger.Log.Info("Starting account age command")
-	// Acknowledge the command immediately with a deferred response.
 	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
 	})
@@ -103,20 +93,17 @@ func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		logger.Log.WithError(err).Error("Error sending deferred response")
 		return
 	}
-	// Get the user ID and account ID from the interaction.
 	userID := i.Member.User.ID
-	accountId := i.ApplicationCommandData().Options[0].IntValue()
-	logger.Log.Infof("User ID: %s, Account ID: %d", userID, accountId)
+	AccountID := i.ApplicationCommandData().Options[0].IntValue()
+	logger.Log.Infof("User ID: %s, Account ID: %d", userID, AccountID)
 
-	// Get the account from the database.
 	var account models.Account
-	database.DB.Where("id = ?", accountId).First(&account)
+	database.DB.Where("id = ?", AccountID).First(&account)
 	logger.Log.Infof("Account: %+v", account)
 
-	// If the account does not belong to the user, log a warning and return.
 	if account.UserID != userID {
 		logger.Log.WithFields(map[string]interface{}{
-			"account_id": accountId,
+			"account_id": AccountID,
 			"user_id":    userID,
 		}).Warn("User tried to check age for account they don't own")
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -129,8 +116,6 @@ func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		return
 	}
 
-	// Verify the SSO cookie.
-	// Handle the error case (e.g., send a notification to the user)
 	statusCode, err := services.VerifySSOCookie(account.SSOCookie)
 	if err != nil {
 		logger.Log.WithError(err).Errorf("Error verifying SSO cookie for account %s", account.Title)
@@ -144,8 +129,6 @@ func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		return
 	}
 
-	// If the status code is not 200, the SSO cookie is invalid.
-	// Handle the invalid cookie case (e.g., send a notification to the user, mark the account as having an expired cookie)
 	if statusCode != 200 {
 		logger.Log.Errorf("Invalid SSO cookie for account %s", account.Title)
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -158,7 +141,6 @@ func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		return
 	}
 
-	// The SSO cookie is valid, proceed to check the account age.
 	years, months, days, err := services.CheckAccountAge(account.SSOCookie)
 	if err != nil {
 		logger.Log.WithError(err).Errorf("Error checking account age for account %s", account.Title)
@@ -172,14 +154,12 @@ func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		return
 	}
 
-	// Create an embed for the response.
 	embed := &discordgo.MessageEmbed{
 		Title:       fmt.Sprintf("%s - %s", account.Title, account.LastStatus),
 		Description: fmt.Sprintf("The account is %d years, %d months, and %d days old.", years, months, days),
 		Color:       0x00ff00,
 	}
 
-	// Respond to the interaction with the embed.
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
--- command/removeaccount/removeaccount.go
+++ command/removeaccount/removeaccount.go
@@ -2,9 +2,9 @@ package removeaccount
 
 import (
 	"codstatusbot/database"
-	"codstatusbot/internal"
-	"codstatusbot/internal/logger"
+	"codstatusbot/logger"
 	"codstatusbot/models"
+	"codstatusbot/services"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -14,10 +14,7 @@ var choices []*discordgo.ApplicationCommandOptionChoice
 
 // RegisterCommand registers the "removeaccount" command in the Discord session for a specific guild.
 func RegisterCommand(s *discordgo.Session, guildID string) {
-	// Fetch all choices for the guild.
-	choices = internal.GetAllChoices(guildID)
-
-	// Define the command and its options.
+	choices = services.GetAllChoices(guildID)
 	commands := []*discordgo.ApplicationCommand{
 		{
 			Name:        "removeaccount",
@@ -34,14 +31,12 @@ func RegisterCommand(s *discordgo.Session, guildID string) {
 		},
 	}
 
-	// Fetch existing commands.
 	existingCommands, err := s.ApplicationCommands(s.State.User.ID, guildID)
 	if err != nil {
 		logger.Log.WithError(err).Error("Error getting application commands from guild")
 		return
 	}
 
-	// Check if the "removeaccount" command already exists.
 	var existingCommand *discordgo.ApplicationCommand
 	for _, command := range existingCommands {
 		if command.Name == "removeaccount" {
@@ -52,7 +47,6 @@ func RegisterCommand(s *discordgo.Session, guildID string) {
 
 	newCommand := commands[0]
 
-	// If the command exists, update it. Otherwise, create a new one.
 	if existingCommand != nil {
 		logger.Log.Info("Updating the removeaccount command")
 		_, err = s.ApplicationCommandEdit(s.State.User.ID, guildID, existingCommand.ID, newCommand)
@@ -72,14 +66,12 @@ func RegisterCommand(s *discordgo.Session, guildID string) {
 
 // UnregisterCommand removes all application commands from the Discord session for a specific guild.
 func UnregisterCommand(s *discordgo.Session, guildID string) {
-	// Fetch existing commands.
 	commands, err := s.ApplicationCommands(s.State.User.ID, guildID)
 	if err != nil {
 		logger.Log.WithError(err).Error("Error getting application commands for unregistering removeaccount command")
 		return
 	}
 
-	// Delete each command.
 	for _, command := range commands {
 		logger.Log.Infof("Deleting command %s", command.Name)
 		err := s.ApplicationCommandDelete(s.State.User.ID, guildID, command.ID)
@@ -93,13 +85,11 @@ func UnregisterCommand(s *discordgo.Session, guildID string) {
 // CommandRemoveAccount handles the "removeaccount" command when invoked.
 func CommandRemoveAccount(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	logger.Log.Info("Starting remove account command")
-	// Extract the user ID, guild ID, and account ID from the interaction.
 	userID := i.Member.User.ID
 	guildID := i.GuildID
-	accountId := i.ApplicationCommandData().Options[0].IntValue()
-	logger.Log.Infof("User ID: %s, Guild ID: %s  Account ID: %d", userID, guildID, accountId)
+	AccountID := i.ApplicationCommandData().Options[0].IntValue()
+	logger.Log.Infof("User ID: %s, Guild ID: %s  Account ID: %d", userID, guildID, AccountID)
 
-	// Begin a database transaction.
 	tx := database.DB.Begin()
 	defer func() {
 		if r := recover(); r != nil {
@@ -107,56 +97,48 @@ func CommandRemoveAccount(s *discordgo.Session, i *discordgo.InteractionCreate) 
 		}
 	}()
 
-	// Fetch the account from the database.
 	var account models.Account
-	result := tx.Where("user_id = ? AND id = ? AND guild_id = ?", userID, accountId, guildID).First(&account)
+	result := tx.Where("user_id = ? AND id = ? AND guild_id = ?", userID, AccountID, guildID).First(&account)
 	if result.Error != nil {
-		// If the account does not exist, respond with an ephemeral message.
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
 				Content: "Account does not exist",
-				Flags:   64, // Set ephemeral flag
+				Flags:   64,
 			},
 		})
 		tx.Rollback()
 		return
 	}
-	// Disable foreign key constraints.
 	err := tx.Exec("SET FOREIGN_KEY_CHECKS=0;").Error
 	if err != nil {
 		logger.Log.WithError(err).Error("Error disabling foreign key constraints")
 		tx.Rollback()
 		return
 	}
-	defer tx.Exec("SET FOREIGN_KEY_CHECKS=1;") // Re-enable foreign key constraints
+	defer tx.Exec("SET FOREIGN_KEY_CHECKS=1;")
 
-	// Delete associated bans.
 	if err := tx.Unscoped().Where("account_id = ?", account.ID).Delete(&models.Ban{}).Error; err != nil {
 		logger.Log.WithError(err).Error("Error deleting associated bans for account", account.ID)
 		tx.Rollback()
 		return
 	}
 
-	// Delete the account.
 	if err := tx.Unscoped().Where("id = ?", account.ID).Delete(&models.Account{}).Error; err != nil {
 		logger.Log.WithError(err).Error("Error deleting account from database", account.ID)
 		tx.Rollback()
 		return
 	}
 
-	// Respond with an ephemeral message indicating success.
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Content: "Account removed",
-			Flags:   64, // Set ephemeral flag
+			Flags:   64,
 		},
 	})
 
-	// Update the choices for the "removeaccount" command.
 	UpdateAccountChoices(s, guildID)
-	// Commit the transaction.
 	tx.Commit()
 	logger.Log.Info("Account removed successfully")
 }
@@ -165,17 +147,14 @@ func CommandRemoveAccount(s *discordgo.Session, i *discordgo.InteractionCreate) 
 func UpdateAccountChoices(s *discordgo.Session, guildID string) {
 	logger.Log.Info("Updating account choices")
 
-	// Fetch all choices for the guild.
-	choices = internal.GetAllChoices(guildID)
+	choices = services.GetAllChoices(guildID)
 
-	// Fetch existing commands.
 	commands, err := s.ApplicationCommands(s.State.User.ID, guildID)
 	if err != nil {
 		logger.Log.WithError(err).Error("Error getting application command choices")
 		return
 	}
 
-	// Define the configurations for the commands that need to be updated.
 	commandConfigs := map[string]*discordgo.ApplicationCommand{
 		"removeaccount": {
 			Name:        "removeaccount",
@@ -237,7 +216,6 @@ func UpdateAccountChoices(s *discordgo.Session, guildID string) {
 		},
 	}
 
-	// Update each command.
 	for _, command := range commands {
 		if config, ok := commandConfigs[command.Name]; ok {
 			logger.Log.Infof("Updating command %s", command.Name)
