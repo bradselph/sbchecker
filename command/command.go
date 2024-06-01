6a1f6048d2b8965184e1c85cb665068a0a3db105
bradselph <Bselph512@gmail.com>
2024-06-01 16:55:47 -0400

    feat: create command.go, helpcommand.go and 2024-06-01.txt

---
+++ command/command.go
@@ -0,0 +1,69 @@
+package command
+
+import (
+	"codstatusbot2.0/logger"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	CommandHandlers = map[string]func(*discordgo.Session, *discordgo.InteractionCreate){}
+	// Commands        = make(map[string]*discordgo.ApplicationCommand)
+)
+
+func RegisterCommands(s *discordgo.Session, guildID string) {
+	logger.Log.Info("Registering commands by command handler")
+
+	//	addaccount.RegisterCommand(s, guildID, Commands)
+	//	CommandHandlers["addaccount"] = addaccount.CommandAddAccount
+	//	logger.Log.Info("Registering addaccount command")
+
+	//	removeaccount.RegisterCommand(s, guildID, Commands)
+	//	CommandHandlers["removeaccount"] = removeaccount.CommandRemoveAccount
+	//	logger.Log.Info("Registering removeaccount command")
+
+	//	accountlogs.RegisterCommand(s, guildID, Commands)
+	//	CommandHandlers["accountlogs"] = accountlogs.CommandAccountLogs
+	//	logger.Log.Info("Registering accountlogs command")
+
+	//	updateaccount.RegisterCommand(s, guildID, Commands)
+	//	CommandHandlers["updateaccount"] = updateaccount.CommandUpdateAccount
+	//	logger.Log.Info("Registering updateaccount command")
+
+	//	accountage.RegisterCommand(s, guildID, Commands)
+	//	CommandHandlers["accountage"] = accountage.CommandAccountAge
+	//	logger.Log.Info("Registering accountage command")
+
+	help.registerCommand(s, guildID, command)
+	CommandHandlers["help"] = help.CommandHelp
+	logger.Log.Info("Registering help command")
+}
+
+/*
+func UnregisterCommands(s *discordgo.Session, guildID string) {
+	logger.Log.Info("Unregistering commands by command handler")
+
+	addaccount.UnregisterCommand(s, guildID)
+	logger.Log.Info("Unregistering addaccount command")
+
+	removeaccount.UnregisterCommand(s, guildID)
+	logger.Log.Info("Unregistering removeaccount command")
+
+	accountlogs.UnregisterCommand(s, guildID)
+	logger.Log.Info("Unregistering accountlogs command")
+
+	updateaccount.UnregisterCommand(s, guildID)
+	logger.Log.Info("Unregistering updateaccount command")
+
+	accountage.UnregisterCommand(s, guildID)
+	logger.Log.Info("Unregistering accountage command")
+
+	help.UnregisterCommand(s, guildID)
+	logger.Log.Info("Unregistering help command")
+
+	err := s.BulkOverwriteGuildCommands(guildID, []*discordgo.ApplicationCommand{})
+	if err != nil {
+		logger.Log.WithError(err).Error("Error unregistering commands")
+	}
+}
+*/
---
+++ command/help/helpcommand.go
@@ -0,0 +1,91 @@
+package help
+
+import (
+	"codstatusbot2.0/logger"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func registerCommand(s *discordgo.Session, guildID string) {
+	commands := []*discordgo.ApplicationCommand{
+		{
+			Name:        "help",
+			Description: "Get help or report an issue",
+		},
+	}
+
+	existingCommands, err := s.ApplicationCommands(s.State.User.ID, guildID)
+	if err != nil {
+		logger.Log.WithError(err).Error("Error getting application commands")
+		return
+	}
+
+	var existingCommand *discordgo.ApplicationCommand
+	for _, command := range existingCommands {
+		if command.Name == "help" {
+			existingCommand = command
+			break
+		}
+	}
+
+	newCommand := commands[0]
+
+	if existingCommand != nil {
+		logger.Log.Info("Updating help command")
+		_, err = s.ApplicationCommandEdit(s.State.User.ID, guildID, existingCommand.ID, newCommand)
+		if err != nil {
+			logger.Log.WithError(err).Error("Error updating help command")
+			return
+		}
+	} else {
+		logger.Log.Info("Creating help command")
+		_, err = s.ApplicationCommandCreate(s.State.User.ID, guildID, newCommand)
+		if err != nil {
+			logger.Log.WithError(err).Error("Error creating help command")
+			return
+		}
+	}
+}
+
+/*
+// UnregisterCommand deletes all application commands for a given guild.
+func UnregisterCommand(s *discordgo.Session, guildID string) {
+	commands, err := s.ApplicationCommands(s.State.User.ID, guildID)
+	if err != nil {
+		logger.Log.WithError(err).Error("Error getting application commands")
+		return
+	}
+
+	for _, command := range commands {
+		logger.Log.Infof("Deleting command %s", command.Name)
+		err := s.ApplicationCommandDelete(s.State.User.ID, guildID, command.ID)
+		if err != nil {
+
+			logger.Log.WithError(err).Errorf("Error deleting command %s", command.Name)
+
+			return
+		}
+	}
+}
+*/
+/*
+// CommandHelp handles the "help" command.
+func CommandHelp(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	logger.Log.Info("Received help command")
+	embed := &discordgo.MessageEmbed{
+		Title: "Help",
+
+		Description: "If you need help or have any issues, please message Susplayer32 on Discord. ",
+
+		Color: 0x00ff00,
+	}
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{embed},
+			Flags:  64,
+		},
+	})
+}
+*/
