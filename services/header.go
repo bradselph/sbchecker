c9ad0e4edf92715ab1b745442cc71408abb338f3
bradselph <Bselph512@gmail.com>
2024-06-27 14:32:11 -0400

    Update header.go

--- services/header.go
+++ services/header.go
@@ -6,9 +6,15 @@ import (
 
 func GenerateHeaders(ssoCookie string) map[string]string {
 	return map[string]string{
-		"accept":           "*/*",
-		"sec-fetch-mode":   "cors",
-		"x-requested-with": "XMLHttpRequest",
-		"cookie":           fmt.Sprintf("ACT_SSO_COOKIE=%s", ssoCookie),
+		"accept":         "*/*",
+		"sec-fetch-mode": "cors",
+		"cookie":         fmt.Sprintf("ACT_SSO_COOKIE=%s", ssoCookie),
 	}
+}
+
+func GeneratePostHeaders(ssoCookie string) map[string]string {
+	headers := GenerateHeaders(ssoCookie)
+	headers["Content-Type"] = "application/x-www-form-urlencoded"
+	headers["x-requested-with"] = "XMLHttpRequest"
+	return headers
 }
